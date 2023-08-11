f44a9a112252b80d3a245973623452bc2ec19ef3
Dmitry-664 <rogachev-64@mail.ru>
2023-08-11 11:33:19 +0300

    9-й спринт

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,73 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_QUERY = "SEARCH_QUERY"
+    }
+    private lateinit var inputEditText: EditText
+    private var currentSearchQuery: String = ""
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_QUERY, currentSearchQuery)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        if (savedInstanceState != null) {
+            currentSearchQuery = savedInstanceState.getString(SEARCH_QUERY, "")
+            inputEditText.setText(currentSearchQuery)
+        }
+
+        val backButton = findViewById<Button>(R.id.buttonBack)
+        backButton.setOnClickListener{
+            finish()
+        }
+
+        inputEditText = findViewById<EditText>(R.id.inputEditText)
+        inputEditText.setOnClickListener{
+        }
+
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        clearButton.visibility = View.INVISIBLE
+        clearButton.setOnClickListener{
+            inputEditText.text.clear()
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+            inputEditText.clearFocus()
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int)=Unit
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                currentSearchQuery = inputEditText.text.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?)=Unit
+        }
+        inputEditText.addTextChangedListener(textWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.INVISIBLE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,19 +1,78 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.example.playlistmaker.R
 
 class SettingsActivity : AppCompatActivity() {
+    private var isDarkThemeEnabled = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         val backButton = findViewById<Button>(R.id.buttonBack)
         backButton.setOnClickListener {
             finish()
-
         }
+        val switchButton = findViewById<SwitchCompat>(R.id.switchButton)
+        switchButton.setOnCheckedChangeListener { _, isChecked ->
+            isDarkThemeEnabled = isChecked
+            applyTheme()
+        }
+        val shareButton = findViewById<Button>(R.id.buttonShare)
+        shareButton.setOnClickListener{
+            shareApp()
+        }
+        val supportWrite = findViewById<Button>(R.id.writeToSupport)
+        supportWrite.setOnClickListener{
+            supportApp()
+        }
+        val agreementUser = findViewById<Button>(R.id.user_agreement)
+        agreementUser.setOnClickListener{
+            agreementApp()
+        }
+    }
+
+    private fun applyTheme() {
+        if (isDarkThemeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+    private fun shareApp() {
+        val shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        shareIntent.type = "text/plain"
+        val shareMessage = "Курсы от Яндекс-Практикума: " +
+                "https://practicum.yandex.ru/"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage)
+        startActivity(Intent(shareIntent))
+    }
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun supportApp() {
+        val topic = getString(R.string.support_topic)
+        val message = getString(R.string.support_text)
+        val intent = Intent(Intent.ACTION_SENDTO)
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email)))
+        intent.putExtra(Intent.EXTRA_SUBJECT, topic)
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+    private fun agreementApp() {
+        val url = getString(R.string.yandex_offer)
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
     }
 }
