b59b9b85f4802a0ba907c00239ddcbc382df7eae
Dmitry-664 <rogachev-64@mail.ru>
2023-10-03 10:43:48 +0300

    Домашнее задание 12-го спринта ( финишное. Верное )

--- app/src/main/java/com/example/playlistmaker/SearchAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/SearchAdapter.kt
@@ -8,15 +8,9 @@ import androidx.recyclerview.widget.RecyclerView
 import java.util.ArrayList
 
 class SearchAdapter(
-            private var trackList: MutableList<Track>,
-            private val itemClickListener: (Track) -> Unit
+    private var trackList: MutableList<Track>,
+    private val itemClickListener: (Track) -> Unit
 ) : RecyclerView.Adapter<SearchViewHolder>() {
-//    fun setTracks(newTracks: MutableList<Track>) {
-//        val diffResult = DiffUtil.calculateDiff(TrackDiffCallback(trackList, newTracks))
-//        trackList.clear()
-//        trackList.addAll(newTracks)
-//        diffResult.dispatchUpdatesTo(this)
-//    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
         val view =
@@ -36,22 +30,4 @@ class SearchAdapter(
     override fun getItemCount(): Int {
         return trackList.size
     }
-//    private class TrackDiffCallback : DiffUtil.ItemCallback<Track>() {
-//
-////        override fun getOldListSize(): Int {
-////            return oldList.size
-////        }
-////
-////        override fun getNewListSize(): Int {
-////            return newList.size
-////        }
-//
-//        override fun areItemsTheSame(oldItem: Track, newItem: Track): Boolean {
-//            return oldItem.trackId == newItem.trackId
-//        }
-//
-//        override fun areContentsTheSame(oldItem: Track, newItem: Track): Boolean {
-//            return oldItem == newItem
-//        }
-//    }
 }
