9fc64767c40b206f98a136e1aea98b9e0af88c4f
Dmitry-664 <rogachev-64@mail.ru>
2023-12-19 14:31:03 +0300

    16-й спринт (Домашнее задание - MVVM) - Финишное - правильно работающее.

--- app/src/main/java/com/example/playlistmaker/ui/search/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/search/SearchActivity.kt
@@ -103,7 +103,7 @@ class SearchActivity : AppCompatActivity() {
                 Unit
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                clearButton.isVisible = p0.isNullOrEmpty()
+                clearButton.visibility = clearButtonVisibility(p0)
                 currentSearchQuery = inputEditText.text.toString()
                 val queryNew = p0?.toString().orEmpty()
                 currentSearchQuery = queryNew
@@ -231,5 +231,11 @@ class SearchActivity : AppCompatActivity() {
         noConnectSearch.isVisible = false
         searchHistoryLayout.isVisible = false
     }
-
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
 }
